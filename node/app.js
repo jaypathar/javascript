8d161369c991be980f84bec5d3f27b27a20215a1
jaypathar <jayp.albiorix@gmail.com>
2023-06-27 11:25:57 +0530

    change as requested

--- node/app.js
+++ node/app.js
@@ -1,1 +1,15 @@
-console.log("Hello")
+const express = require('experss');
+const app = express();
+let PORT =5000
+
+app.get('/',(req,res)=>{
+    res.send("I am a server")
+})
+
+const start = async ()=>{
+    try{
+        app.listen(PORT,()=>{
+            console.log(`I am live on port ${PORT}`)
+        })
+    }catch (error){}
+}
