c9cb7f84c4a3375134e0aacc8975a78fecadf811
jaypathar <jayp.albiorix@gmail.com>
2023-07-14 17:42:44 +0530

    added functionality to todo list

--- Mini Projects/Todo app/script.js
+++ Mini Projects/Todo app/script.js
@@ -1,4 +1,4 @@
-// Define a class for the submitted information
+// Define a class for the submitted information.
 class Information {
   constructor(title, description) {
     this.title = title;
@@ -6,15 +6,22 @@ class Information {
   }
 }
 
-// Function to handle form submission
-function handleSubmit(event) {
-  event.preventDefault(); // Prevent form from refreshing the page
+// Check if the list is empty initially.
+const list = document.getElementById("infoList");
+const noItemMessage = document.getElementById("noItemMessage");
+if (list.children.length === 0) {
+  noItemMessage.style.display = "block";
+}
 
-  // Get input values from the form
+// Function to handle form submission.
+function handleSubmit(event) {
+  event.preventDefault(); // Prevent form from refreshing the page.
+
+  // Get input values from the form.
   const title = document.getElementById("titleInput").value;
   const description = document.getElementById("descriptionInput").value;
 
-  // Create an instance of the Information class
+  // Create an instance of the Information class.
   const info = new Information(title, description);
 
   // Display the submitted information in the list
@@ -27,15 +34,22 @@ function handleSubmit(event) {
   `;
   list.appendChild(listItem);
 
-  // Reset the form inputs
+  // Resetting the form inputs.
   document.getElementById("myForm").reset();
 
-  // Add event listener to the delete button in the appended list item
+  // hide "No item in the list" message.
+  const noItemMessage = document.getElementById("noItemMessage");
+  noItemMessage.style.display = "none";
+
+  // Add event listener to the delete button in the appended list item.
   const deleteBtn = listItem.querySelector(".delete-btn");
   deleteBtn.addEventListener("click", function () {
-    listItem.remove(); // Delete the list item when the delete button is clicked
+    listItem.remove(); // Delete the list item on click of delete button.
+    if (list.children.length === 0) {
+      noItemMessage.style.display = "block";
+    }
   });
 }
 
-// Attach event listener to the form
+// Attach event listener to the form.
 document.getElementById("myForm").addEventListener("submit", handleSubmit);
